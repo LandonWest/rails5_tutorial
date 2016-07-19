739dbf829037126a0467b3af67f4cf96d92e448c
Landon West <landonwest5@gmail.com>
2016-07-19 10:55:35 -0700

    refactor dynamic title w/ helper method; refactor test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Reutrns full title on a per-page basis
+  def full_title(page_titile = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_titile.empty?
+      base_title
+    else
+      page_titile + ' | ' + base_title
+    end
+  end
 end
