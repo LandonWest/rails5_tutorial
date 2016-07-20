406e14c932e2167f7260aab4a745e66ee8021026
Landon West <landonwest5@gmail.com>
2016-07-20 13:30:24 -0700

    fix typos in full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   # Reutrns full title on a per-page basis
-  def full_title(page_titile = '')
+  def full_title(page_title = '')
     base_title = 'Ruby on Rails Tutorial Sample App'
-    if page_titile.empty?
+    if page_title.empty?
       base_title
     else
-      page_titile + ' | ' + base_title
+      page_title + ' | ' + base_title
     end
   end
 end
