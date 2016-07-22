cb677eedfd5bb09170b9b03ca8729852ad5b5907
Landon West <landonwest5@gmail.com>
2016-07-22 16:44:34 -0700

    create sessions controller; modify routes; update tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'static_pages#home'
-  get '/help',    to: 'static_pages#help'#, as: 'helf'  # the Far Side comic strip joke! changes route helper name
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
+
+  get '/help',      to: 'static_pages#help'#, as: 'helf'  # the Far Side comic strip joke! changes route helper name
+  get '/about',     to: 'static_pages#about'
+  get '/contact',   to: 'static_pages#contact'
+  get '/signup',    to: 'users#new'
+  post '/signup',   to: 'users#create'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users, :except => [:new, :create]
 end
