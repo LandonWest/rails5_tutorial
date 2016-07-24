8753b643a1d7764618b7db39811c22cad72c67cf
Landon West <landonwest5@gmail.com>
2016-07-24 12:11:33 -0700

    create sessions helpers for login/current_user; refactor nav links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # login
+      log_in user
+      redirect_to user  # ( user_url(user) )
     else
       flash.now[:danger] = 'Invalid email or password'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  # logs in the current user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # returns the currently logged in user (if any)
+  def current_user
+    @curent_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the current user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
 end
