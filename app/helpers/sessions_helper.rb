17f345585bec658ae99a60a3586b1a81711397de
Landon West <landonwest5@gmail.com>
2016-07-25 21:19:35 -0700

    create remember token and digest to allow permanent session cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # returns the currently logged in user (if any)
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # returns the user corresponding to the remember token cookie.
   def current_user
-    @curent_user ||= User.find_by(id: session[:user_id])
+    # this "=" is not a comparison. read: “If session of user id exists (while setting user id to session of user id)…”
+    if (user_id = session[:user_id])
+      @curent_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])   # dito
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # returns true if the current user is logged in
