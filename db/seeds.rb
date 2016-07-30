dc3b7ca2ca04063a04993b8290f2faca8ff66e4e
Landon West <landonwest5@gmail.com>
2016-07-29 20:35:45 -0700

    add admin to user; create destroy action with admin before filter; add tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobarbaz",
-             password_confirmation: "foobarbaz")
+             password_confirmation: "foobarbaz",
+             admin: true)
 
 99.times do |n|
   name     = Faker::Name.name
