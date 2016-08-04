a7ef8a53dfd8b182b9b66927522fad44a8b88527
Landon West <landonwest5@gmail.com>
2016-08-03 18:32:05 -0700

    test for signin with account activation; refactor User controller / add activate and send_activation_email methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)  # self is optional inside the model. (self.update_attribute(...))
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private # private methods are only available on the class object itself, not on instantiated objects of that class
 
     def downcase_email
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
   test 'invalid sign up information submitted' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -16,18 +19,35 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
-  test 'valid signup' do
+  test 'valid signup information with account activation' do
     get signup_path
-    assert_difference 'User.count' do
+    assert_difference 'User.count', 1 do
       post signup_path, params: { user: { name: 'Example User',
                                          email: 'user@example.com',
                                          password: 'foobarbaz',
                                          password_confirmation: 'foobarbaz' } }
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user) # assigns lets us access instance variables in the corresponding action
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path('invalid token', email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
-    # assert_template 'users/show'
+    assert_template 'users/show'
+    assert is_logged_in?
     # assert_select 'div.alert.alert-success'  # would be brittle as the flash key or div might change
     assert_not flash.empty?
-    # assert is_logged_in?
+
   end
 end
