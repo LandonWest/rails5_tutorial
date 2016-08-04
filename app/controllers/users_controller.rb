534309e2fe4027de7e26ce0374fa30ccd366d38c
Landon West <landonwest5@gmail.com>
2016-08-03 18:44:12 -0700

    only show activated users in index; redirect unactivated user on show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: [:destroy]
 
-  def index
-    @users = User.paginate(page: params[:page]) # params[:page] is generated automatically by will_paginate gem
+  def index      # params[:page] is generated automatically by will_paginate gem
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -43,8 +43,7 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_attribute(:activated, true)  # self is optional inside the model. (self.update_attribute(...))
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Sends activation email.
