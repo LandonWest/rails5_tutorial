9849b89b1b782cec3db7700504930bfdc46b87c5
Landon West <landonwest5@gmail.com>
2016-08-08 21:56:21 -0700

    add file upload gems; add picture uploader and test

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) } # “stabby lambda” syntax ( -> )
   # for an object called a Proc (procedure) or lambda, which is an anonymous function
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
